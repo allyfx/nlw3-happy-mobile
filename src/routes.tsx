3b421a07d52fb0da2f4b2e2fed42a053c6c1ee5b
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-16 13:53:07 -0300

    Two new routes added

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,6 +8,9 @@ const { Navigator, Screen } = createStackNavigator();
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
 
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+
 export default function Routes() {
     return (
         <NavigationContainer>
@@ -15,6 +18,9 @@ export default function Routes() {
                 <Screen name="OrphanagesMap" component={OrphanagesMap} />
 
                 <Screen name="OrphanageDetails" component={OrphanageDetails} />
+
+                <Screen name="SelectMapPosition" component={SelectMapPosition} />
+                <Screen name="OrphanageData" component={OrphanageData} />
             </Navigator>
         </NavigationContainer>
     );
